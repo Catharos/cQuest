0800297ae4215e4f59acfa3b48150bc70e6c756b
SpaceEmotion <spaceemotion@gmail.com>
2012-12-06 08:38:41 +0100

    Added pom.xml and basic plugin files

---
+++ src/main/java/net/catharos/cquest/cQuest.java
@@ -0,0 +1,17 @@
+package net.catharos.cquest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class cQuest extends JavaPlugin {
+	/** Enables the plugin */
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	/** Disables the plugin */
+	@Override
+	public void onDisable() {
+		
+	}
+}
